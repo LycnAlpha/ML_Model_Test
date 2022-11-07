fd6b22518865545ad331bc4aed565131c04ae98e
LycnAlpha <shashithdperera@gmail.com>
2022-11-07 16:08:31 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,104 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:mltest/main.dart';
+import 'package:tflite/tflite.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output = "";
+  int _imageCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+    loadCamera();
+  }
+
+  loadCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((ImageStream) async {
+            cameraImage = ImageStream;
+            _imageCount++;
+            if (_imageCount % 30 == 0) {
+              _imageCount = 0;
+              runModel();
+            }
+
+            //await Tflite.close();
+          });
+        });
+      }
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+        bytesList: cameraImage!.planes.map((Plane) {
+          return Plane.bytes;
+        }).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
+
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+        });
+      });
+    }
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Learning Center")),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: !cameraController!.value.isInitialized
+                  ? Container()
+                  : AspectRatio(
+                      aspectRatio: cameraController!.value.aspectRatio,
+                      child: CameraPreview(cameraController!),
+                    ),
+            ),
+          ),
+          Text(
+            output,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          )
+        ],
+      ),
+    );
+  }
+}
